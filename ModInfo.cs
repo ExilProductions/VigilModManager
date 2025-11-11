6138ce080eea213682ba4c1d1c53307e7b5f858f
Exil Productions <exil.productions.business@gmail.com>
2025-11-11 16:58:04 +0100

    Add Modinfo Class

---
+++ ModInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VMM
+{
+    internal class ModInfo
+    {
+        public const string Name = "VigilModManager";
+        public const string Version = "1.0.0";
+        public const string Author = "Exil_S";
+        public const string DownloadLink = "";
+    }
+}
